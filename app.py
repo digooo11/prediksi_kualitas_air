4f4c029582fe2200c0a6257f1b1591b897f7d5d7
digooo11 <101240007+digooo11@users.noreply.github.com>
2022-06-22 14:05:46 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('model1.pkl', 'rb'))
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/predict" , methods=["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    feature = [np.array(float_features)]
+    prediction = model.predict(feature)
+    return render_template("index.html", prediction_text = "{}".format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
